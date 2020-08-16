39f8bb5bf998321021bd32a3dde16e5743ef813a
hubentu <huqmail@gmail.com>
2020-08-16 10:16:35 -0400

    Fix check problem

--- R/python.R
+++ R/python.R
@@ -8,11 +8,25 @@
     }
 })
 
-.namespace <- py_run_string("
+.namespace <- local({
+    .namespace <- NULL
+    function() {
+        if (is.null(.namespace)){
+            .namespace <<- py_run_string("
 class Namespace:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 ")
+        }
+        .namespace
+    }
+})
+
+## .namespace <<- py_run_string("
+## class Namespace:
+##     def __init__(self, **kwargs):
+##         self.__dict__.update(kwargs)
+## ")
 
 .filterdup <- local({
     .filterdup <- NULL
