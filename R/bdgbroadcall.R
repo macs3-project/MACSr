b520b6e4f4d5ac8fb1ec009bed505b78978b6aeb
hubentu <qiang.hu@roswellpark.org>
2020-12-06 21:23:21 -0500

    bdgbroadcall

---
+++ R/bdgbroadcall.R
@@ -0,0 +1,55 @@
+#' bdgbroadcall
+#'
+#' Call broad peaks from bedGraph output. Note: All regions on the
+#' same chromosome in the bedGraph file should be continuous so only
+#' bedGraph files from MACS3 are accpetable.
+#'
+#' @param ifile MACS score in bedGraph. REQUIRED.
+#' @param cutoffpeak Cutoff for peaks depending on which method you
+#'     used for score track. If the file contains qvalue scores from
+#'     MACS3, score 2 means qvalue 0.01. DEFAULT: 2
+#' @param cutofflink Cutoff for linking regions/low abundance regions
+#'     depending on which method you used for score track. If the file
+#'     contains qvalue scores from MACS3, score 1 means qvalue 0.1,
+#'     and score 0.3 means qvalue 0.5. DEFAULT: 1", default = 1
+#' @param minlen minimum length of peak, better to set it as d value. DEFAULT: 200",
+#'     default = 200
+#' @param lvl1maxgap maximum gap between significant peaks, better to
+#'     set it as tag size. DEFAULT: 30
+#' @param lvl2maxgap maximum linking between significant peaks, better
+#'     to set it as 4 times of d value. DEFAULT: 800
+#' @param trackline Tells MACS not to include trackline with bedGraph
+#'     files. The trackline is required by UCSC.
+#' @param outputfile The output file.
+#' @param outdir The output directory.
+#' @param log Whether to capture logs.
+#' @examples
+#' \dontrun{
+#' bdgbroadcall("run_bdgcmp_FE.bdg", cutoffpeak = 2, cutofflink = 1.5)
+#' }
+bdgbroadcall <- function(ifile, cutoffpeak = 2, cutofflink = 1,
+                         minlen = 200L, lvl1maxgap = 30L,
+                         lvl2maxgap = 800L, trackline = TRUE,
+                         outdir = ".", outputfile = character(),
+                         log = TRUE){
+    opts <- .namespace()$Namespace(ifile = ifile,
+                                   cutoffpeak = cutoffpeak,
+                                   cutofflink = cutofflink,
+                                   minlen = minlen,
+                                   lvl1maxgap = lvl1maxgap,
+                                   lvl2maxgap = lvl2maxgap,
+                                   trackline = trackline,
+                                   outdir = outdir,
+                                   ofile = outputfile)
+    if(log){
+        .logging()$run()
+        res <- py_capture_output(.bdgbroadcall()$run(opts))
+        message(res)
+    }else{
+        res <- .bdgbroadcall()$run(opts)
+    }
+
+    ofile <- file.path(outdir, outputfile)
+    args <- as.list(match.call())
+    macsList(fun = args[[1]], arguments = args[-1], outputs = ofile, log = res)
+}
