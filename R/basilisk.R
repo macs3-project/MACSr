186a15b2fd7bd839ed819683d96a49e8abf49e96
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-06-08 17:20:06 +0100

    Remove >=
    basilisk no longer allows >/< when specifying package versions, only = or ==
    https://github.com/LTLA/basilisk/issues/17

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,4 @@
 #' @import basilisk
 env_macs <- BasiliskEnvironment("env_macs", pkgname="MACSr",
-                                packages = c("numpy>=1.17"),
+                                packages = c("numpy=1.17"),
                                 pip = c("macs3==3.0.0a6"))
