9b5d5b96a3f32cc404ac5c077da1b91183f54683
hubentu <qiang.hu@roswellpark.org>
2022-09-20 16:19:06 -0400

    update basilisk

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,4 @@
 #' @import basilisk
 env_macs <- BasiliskEnvironment("env_macs", pkgname="MACSr",
-                                packages = c("numpy=1.17"),
-                                pip = c("macs3==3.0.0a6"))
+                                packages = c("python=3.8"),
+                                pip = c("macs3==3.0.0a7"))
