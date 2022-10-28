09875eac41671ec00deada415a707de53c81a6b5
qhu <qiang.hu@roswellpark.org>
2022-10-28 10:09:24 -0400

    add hmmratac

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,4 @@
 #' @import basilisk
 env_macs <- BasiliskEnvironment("env_macs", pkgname="MACSr",
-                                packages = c("python=3.8"),
-                                pip = c("macs3==3.0.0a7"))
+                                packages = c("python=3.10"),
+                                pip = c("macs3==3.0.0b1"))
